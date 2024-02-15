5c28d22b12094e6053873de17642a70065c44176
mosmn <ithinkisee8@gmail.com>
2024-02-15 12:25:59 +0800

    Refactor login function in auth.jsx

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -1,23 +1,26 @@
 const url = 'http://localhost:3000';
-
+    
 export const login = async (formData) => {
   try {
     const response = await fetch(`${url}/auth/login`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
     });
-  
+
     const data = await response.json();
 
-    localStorage.setItem('token', data.token);
-    
-    return data;
+    if (response.ok) {
+      localStorage.setItem('token', data.token);
+      return data.payload;
+    }
+
+    throw new Error(data.message);
   } catch (error) {
     console.error(error);
+    return null;
   }
-}
-    
+};
