2bacdc73a619eb0aa2c271094a19eb0cd7d5eb99
mosmn <ithinkisee8@gmail.com>
2024-02-23 15:40:57 +0800

    Refactor routes in index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../components/Login';
-import Dashboard from '../components/Dashboard';
-import PostEditor from '../components/PostEditor';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../components/Login";
+import Dashboard from "../components/Dashboard";
+import PostEditor from "../components/PostEditor";
 
 const AppRoutes = () => (
-    <Router basename="/blog/admin">
-        <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
-        <Route path="/post-editor" element={<PostEditor />}>
-            <Route path=":id" element={<PostEditor />} />
-        </Route>
-        </Routes>
-    </Router>
+  <Router basename="/blog/admin">
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/post-editor" element={<PostEditor />}>
+        <Route path=":id" element={<PostEditor />} />
+      </Route>
+    </Routes>
+  </Router>
 );
 
 export default AppRoutes;
