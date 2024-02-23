cbc79a5f299838d742ef7e8f817df914fb871180
mosmn <ithinkisee8@gmail.com>
2024-02-23 15:57:39 +0800

    Add redirect to dashboard drafts

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom";
 import Login from "../components/Login";
 import Dashboard from "../components/Dashboard";
 import PostEditor from "../components/PostEditor";
@@ -6,6 +6,9 @@ import PostEditor from "../components/PostEditor";
 const AppRoutes = () => (
   <Router basename="/blog/admin">
     <Routes>
+    <Route exact path="/">
+        <Redirect to="/blog/admin/dashboard/drafts" />
+      </Route>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/post-editor" element={<PostEditor />}>
