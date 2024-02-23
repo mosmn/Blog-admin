9c1184a1f71a84e3977ca741ed5db71d3cb1a07e
mosmn <ithinkisee8@gmail.com>
2024-02-24 07:18:45 +0800

    Remove RedirectToDrafts component from AppRoutes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,23 +2,11 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "../components/Login";
 import Dashboard from "../components/Dashboard";
 import PostEditor from "../components/PostEditor";
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-function RedirectToDrafts() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate('/blog/admin/dashboard/drafts');
-  }, [navigate]);
-
-  return null;
-}
 
 
 const AppRoutes = () => (
   <Router basename="/blog/admin">
     <Routes>
-    <Route path="/" element={<RedirectToDrafts />} />
       <Route path="/" element={<Login />} />
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/post-editor" element={<PostEditor />}>
