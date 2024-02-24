39bf135328131f17793ec60ab1c7b9626356182c
mosmn <ithinkisee8@gmail.com>
2024-02-24 09:10:46 +0800

    Add authentication check for dashboard and post editor routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,27 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import Login from "../components/Login";
 import Dashboard from "../components/Dashboard";
 import PostEditor from "../components/PostEditor";
 
+const isLoggedIn = () => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return false;
+  }
+
+  return true;
+};
 
 const AppRoutes = () => (
   <Router basename="/blog/admin">
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard/*" element={<Dashboard />} />
-      <Route path="/post-editor" element={<PostEditor />}>
+      <Route
+        path="/dashboard/*"
+        element={isLoggedIn() ? <Dashboard /> : <Navigate to="/" />}
+      />
+      <Route path="/post-editor" element={isLoggedIn() ? <PostEditor /> : <Navigate to="/" />} >
         <Route path=":id" element={<PostEditor />} />
       </Route>
     </Routes>
