085693ba3c59fe28a1e4750b907d8dbdb163fd82
mosmn <ithinkisee8@gmail.com>
2024-02-24 08:47:19 +0800

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/blog/admin/dashboard/drafts",
   plugins: [react()],
   resolve: {
     alias: {
