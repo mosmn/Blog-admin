66e02dcfcf9ad579f1813e3a7507f6847bd9a882
mosmn <ithinkisee8@gmail.com>
2024-09-21 11:09:33 +0800

    Refactor Vite config to remove dotenv plugin and use import.meta.env for environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import Dotenv from "dotenv";
 
 export default defineConfig({
   base: "/",
-  plugins: [
-    react(),
-    new Dotenv({
-      systemvars: true,
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
